79a178b947851760d82282b287bf5801906b3d83
fatemeh-barati <fatemehbaratit@gmail.com>
2022-07-28 08:58:08 +0430

    add validation for email

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import {Entity, Column, PrimaryGeneratedColumn, BeforeInsert} from 'typeorm';
 
 @Entity()
 export class UserEntity {
@@ -16,4 +16,9 @@ export class UserEntity {
 
     @Column()
     email: string
+
+    @BeforeInsert()
+    emailToLowerCase() {
+        this.email
+    }
 }
